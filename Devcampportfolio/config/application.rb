357bbdae75d9f301d23afb5562c33c3a9d51df9c
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 15:31:25 +0000

    saving application

--- Devcampportfolio/config/application.rb
+++ Devcampportfolio/config/application.rb
@@ -16,13 +16,10 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Devcampportfolio
+module DevcampPortfolio
   class Application < Rails::Application
-   config.generators do |g|
-        g.orm             :active_record
-        g.template_engine :erb
-        g.test_framework  :test_unit, fixture: false
-        g.stylesheets     false
-        g.javascripts     true 
-    end
- end
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
