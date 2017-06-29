24df251acd15ebd47025f8650a192cb3157ef5ac
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 14:34:53 +0000

    added controller concerns for devise params

--- Devcampportfolio/db/schema.rb
+++ Devcampportfolio/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628170816) do
+ActiveRecord::Schema.define(version: 20170107011741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
