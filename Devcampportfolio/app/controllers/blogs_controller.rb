828c2085c0849b957dd098252c05a2ac48526f4b
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 17:51:10 +0000

    implemented authorization in gen for portfolio items and blogs

--- Devcampportfolio/app/controllers/blogs_controller.rb
+++ Devcampportfolio/app/controllers/blogs_controller.rb
@@ -1,6 +1,7 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
 
   # GET /blogs
   # GET /blogs.json
@@ -81,4 +82,4 @@ class BlogsController < ApplicationController
     def blog_params
       params.require(:blog).permit(:title, :body)
     end
-endss
+end
